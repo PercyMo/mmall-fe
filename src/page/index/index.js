2347cb4299467123ebc91c212ca07cdfefab60bb
maobangxin <1418968663@qq.com>
2018-02-03 19:47:18 +0800

    fileheader 注释

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,2 +1,8 @@
+/*
+* @Author: PosyMo
+* @Date:   2018-01-31 19:43:28
+* @Last Modified by:   PosyMo
+* @Last Modified time: 2018-02-03 19:45:56
+*/
 cates = require('./cats.js');
 console.log(cates);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+/*
+* @Author: PosyMo
+* @Date:   2018-02-01 18:46:48
+* @Last Modified by:   PosyMo
+* @Last Modified time: 2018-02-03 19:39:25
+*/
 module.exports = {
     entry: './src/page/index/index.js',
     output: {
         path: './dist',
         filename: 'app.js'
-    }
+    } 
 };
