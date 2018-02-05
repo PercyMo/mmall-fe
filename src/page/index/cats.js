31da59e9d5dd1262db7f345a5ebda0bb017f8fea
maobangxin <1418968663@qq.com>
2018-02-05 19:37:41 +0800

    1

--- src/page/index/cats.js
+++ src/page/index/cats.js
@@ -1,8 +1,2 @@
-/*
-* @Author: PosyMo
-* @Date:   2018-02-01 16:44:54
-* @Last Modified by:   PosyMo
-* @Last Modified time: 2018-02-05 19:37:02
-*/
 var cats = ['dave', 'henry', 'martha'];
 module.exports = cats;
