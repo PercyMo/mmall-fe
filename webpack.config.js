29d406ce7f9de6d78e20d757c1c1b7d3316a5c70
maobangxin <1418968663@qq.com>
2018-02-06 20:10:40 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@
 * @Author: PosyMo
 * @Date:   2018-02-01 18:46:48
 * @Last Modified by:   PosyMo
-* @Last Modified time: 2018-02-03 19:39:25
+* @Last Modified time: 2018-02-06 20:09:47
 */
 module.exports = {
     entry: './src/page/index/index.js',
     output: {
         path: './dist',
-        filename: 'app.js'
-    } 
+        filename: 'app.js' 
+    }
 };
