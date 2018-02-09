88a892330a5e1cdd0a8dd015db7701c23e89af3f
maobangxin <1418968663@qq.com>
2018-02-09 18:16:56 +0800

    html编译打包配置+ejs模板

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,23 @@
 * @Author: PosyMo
 * @Date:   2018-02-01 18:46:48
 * @Last Modified by:   PosyMo
-* @Last Modified time: 2018-02-08 15:09:17
+* @Last Modified time: 2018-02-09 10:09:25
 */
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 获取html-webpack-plugin参数的方法
+var getHtmlConfig = function(name, title) {
+    return {
+        title: title,
+        filename: 'view/' + name + '.html',
+        template: './src/view/' + name + '.html',
+        inject: true,
+        hash: true,
+        chunks: ['common', name]
+    }
+}
 
 // webpack config
 var config = {
@@ -35,6 +48,9 @@ var config = {
         }),
         // 把css单独打包到文件里
         new ExtractTextPlugin("css/[name].css"),
+        // html模板处理
+        new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
     ]
 };
 
