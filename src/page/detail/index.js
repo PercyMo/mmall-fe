9a385f242f9e09ca999edf7800289a569cb43455
maobangxin <1418968663@qq.com>
2018-03-17 10:53:50 +0800

    商品详情页

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -2,7 +2,7 @@
  * @Author: PosyMo 
  * @Date: 2018-03-16 11:44:18 
  * @Last Modified by: PosyMo
- * @Last Modified time: 2018-03-16 15:52:54
+ * @Last Modified time: 2018-03-17 10:52:11
  */
 'use strict';
 require('./index.css');
@@ -10,6 +10,7 @@ require('page/common/nav/index.js');
 require('page/common/header/index.js');
 var _util = require('util/util.js');
 var _product = require('service/product-service.js');
+var _cart = require('service/cart-service.js');
 var templateIndex = require('./index.string');
 
 var page = {
@@ -28,14 +29,45 @@ var page = {
         this.loadDetail();
     },
     bindEvent: function() {
-        
+        var _this = this;
+        // 图片预览
+        $(document).on('mouseenter', '.p-img-item', function() {
+            var imgUrl = $(this).find('.p-img').attr('src');
+            $('.main-img').attr('src', imgUrl);
+        });
+        // count的操作
+        $(document).on('click', '.p-count-btn', function() {
+            var type = $(this).hasClass('plus') ? 'plus' : 'minus',
+                $pCount = $('.p-count'),
+                currCount = parseInt($pCount.val()),
+                minCount = 1,
+                maxCount = _this.data.detailInfo.stock || 1;
+            if (type === 'plus') {
+                $pCount.val(currCount < maxCount ? currCount + 1 : maxCount);
+            } else if (type === 'minus') {
+                $pCount.val(currCount > minCount ? currCount - 1 : minCount);
+            }
+        });
+        // 加入购物车
+        $(document).on('click', '.cart-add', function() {
+            _cart.addToCart({
+                productId: _this.data.productId,
+                count: $('.p-count').val()
+            }, function(res) {
+                window.location.href = './result.html?type=cart-add';
+            }, function(errMsg) {
+                _util.errorTip(errMsg);
+            });
+        });
     },
+    // 加载商品详情的数据
     loadDetail: function() {
         var _this = this,
             html = '',
             $pageWrap = $('.page-wrap');
         _product.getProductDetail(this.data.productId, function(res) {
             _this.filter(res);
+            _this.data.detailInfo = res;
             html = _util.renderHtml(templateIndex, res);
             $pageWrap.html(html);
         }, function(errMsg) {
--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -2,7 +2,7 @@
  * @Author: PosyMo 
  * @Date: 2018-02-28 11:32:05 
  * @Last Modified by: PosyMo
- * @Last Modified time: 2018-02-28 11:42:20
+ * @Last Modified time: 2018-03-17 10:47:16
  */
 'use strict';
 
@@ -13,6 +13,15 @@ var _cart = {
     getCartCount: function(resolve, reject) {
         _util.request({
             url: _util.getServerUrl('/cart/get_cart_product_count.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    // 添加到购物车
+    addToCart: function(productInfo, resolve, reject) {
+        _util.request({
+            url: _util.getServerUrl('/cart/add.do'),
+            data: productInfo,
             success: resolve,
             error: reject
         });
